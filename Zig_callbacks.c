27d5c7fb1c16e792bd0c1d3bb8c80aed7624ccdc
lenloe1 <31710001+lenloe1@users.noreply.github.com>
2020-04-09 16:12:40 -0400

    Initial commit

---
+++ Zig_callbacks.c
@@ -0,0 +1,14 @@
+// This callback file is created for your convenience. You may add application
+// code to this file. If you regenerate this file over a previous version, the
+// previous version will be overwritten and any code you have added will be
+// lost.
+
+#include "app/framework/include/af.h"
+#include "app/framework/plugin/comms-hub-function-sub-ghz/comms-hub-function-sub-ghz-types.h"
+#include "app/framework/util/util.h"    // emberAfGetPageFrom8bitEncodedChanPg()
+#include EMBER_AF_API_NETWORK_STEERING
+
+
+
+
+
